bd5b9d54533a88abe8b4ebec60b78bc32018d460
Administrator <Administrator@A302-7-03>
2025-10-08 11:56:24 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        int result1 = solution.Sum(12, 5);
+        Console.WriteLine(result1);  // 輸出: 17
+
+        int result2 = solution.Sum(-10, 4);
+        Console.WriteLine(result2);  // 輸出: -6
+    }
+}
+
---
+++ Solution.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp3
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("hello, world");
+
+            //Console.VriteLine();
+
+            //string input;
+
+            //input = Console.ReadLine();
+            //input = Console.ReadLine()??"0";
+
+            // int r = Convert.ToInt32(input);
+
+
+
+            String input1;
+
+            input1 = Console.ReadLine();
+
+            int k = Convert.ToInt32(input1);
+
+
+            Console.WriteLine(Sum(r, k));
+
+
+
+
+            int Sum(int num1, int num2)
+            {
+                //1.輸入
+                //int num1, int num2
+
+
+                //2.處理
+                int towNum;
+                towNum = num1 + num2;
+
+                //3.輸出
+                int result;
+                result = towNum;
+
+                return result;
+            }
+        }
